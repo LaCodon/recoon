a61ee3d553eab64d5b7702cb4d11f5e64f9318ec
LaCodon <mf-mm@t-online.de>
2023-04-27 22:31:28 +0200

    restart died containers

--- pkg/controller/event/event.go
+++ pkg/controller/event/event.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	dockertypes "github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/events"
 	dockerclient "github.com/docker/docker/client"
 	metav1 "github.com/lacodon/recoon/pkg/api/v1/meta"
 	projectv1 "github.com/lacodon/recoon/pkg/api/v1/project"
@@ -49,15 +50,14 @@ func (c *Controller) Run(ctx context.Context) error {
 			projectName := event.Actor.Attributes["com.docker.compose.project"]
 
 			switch event.Action {
+			case "die":
+				c.restartContainer(ctx, event.Actor)
 			case "stop":
 				// container stopped
 				fallthrough
-			case "die":
-				// container died
-				fallthrough
 			case "destroy":
 				// container deleted
-				if err := c.triggerReconcile(ctx, projectName); err != nil {
+				if err := c.triggerReconcile(projectName); err != nil {
 					logrus.
 						WithError(err).
 						WithField("project", projectName).
@@ -73,7 +73,7 @@ func (c *Controller) Run(ctx context.Context) error {
 	}
 }
 
-func (c *Controller) triggerReconcile(ctx context.Context, projectName string) error {
+func (c *Controller) triggerReconcile(projectName string) error {
 	project := &projectv1.Project{}
 	if err := c.api.Get(metav1.NamespaceName{
 		Name:      projectName,
@@ -87,4 +87,20 @@ func (c *Controller) triggerReconcile(ctx context.Context, projectName string) e
 	}
 
 	return c.api.Update(project)
+}
+
+func (c *Controller) restartContainer(ctx context.Context, actor events.Actor) {
+	projectName := actor.Attributes["com.docker.compose.project"]
+
+	// load project to make sure restart is really required
+	project := &projectv1.Project{}
+	if err := c.api.Get(metav1.NamespaceName{
+		Name:      projectName,
+		Namespace: "project-" + projectName,
+	}, project); err != nil {
+		return
+	}
+
+	client, _ := dockerclient.NewClientWithOpts(dockerclient.FromEnv, dockerclient.WithAPIVersionNegotiation())
+	_ = client.ContainerStart(ctx, actor.ID, dockertypes.ContainerStartOptions{})
 }
